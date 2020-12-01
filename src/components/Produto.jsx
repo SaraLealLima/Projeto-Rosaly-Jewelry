898506b5dcedba675b9bc91f954a7ad9e9adce31
SaraLealLima <saral.limasm@gmail.com>
2020-11-30 23:31:12 -0300

    aaaaaaaa

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
+import Imagem from './Imagem';
 
 class Produto extends Component {
     constructor(props) {
         super(props);
+        console.log(props)
         this.state = { }
     }
     render() {
         return (
             <div className="box-produto col-xs-6 col-md-6 col-lg-4">
-                <img src="" alt="" />
+                <Imagem src={this.props.produto.imagem} alt= {this.props.produto.nome} />
                 <br/>
-                <p className="nome-produto"></p>
+                <p className="nome-produto">{this.props.produto.nome}</p>
                 <hr/>
-                <p className="antigo-preco"></p>
-                <p className="novo-preco"></p>
+                <p className="antigo-preco">{this.props.produto.preco}</p>
+                <p className="novo-preco">{this.props.produto.novopreco}</p>
             </div>
         )
     }
--- src/paginas/PaginaProdutos.jsx
+++ src/paginas/PaginaProdutos.jsx
@@ -1,20 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import Produto from '../components/Produto';
 
 
-function PaginaProdutos() {
-    const [produtos, setProdutos] = useState([]);
-    useEffect(async () => {
-        const resposta = await fetch("http://rosalyjewelrybackend/api/produtos.php");
-        console.log(resposta);
-        const json = await resposta.json();
-        console.log(json);
-        setProdutos(json);
-    }, [])
-    return <p>
-        {produtos &&  produtos.map(produto => <Produto /> ) }
-    </p>
+class PaginaProdutos extends Component {
+    constructor(props) {
+        super(props)
+        this.state={produtos: [], produtosFiltrados: []};
+    }
 
+    componentDidMount() {
+         this.loadAsyncData()
+    }
+
+    async loadAsyncData() {
+        const resposta = await fetch("http://rosalyjewelrybackend/api/produtos.php");
+        const json = await resposta.json();
+        this.setState({produtos: json, produtosFiltrados: json});
+      }
+
+     exibirCategoria(categoria) {
+        let produtosFiltrados = this.state.produtos.filter((produto) => {
+            return produto.categoria == categoria
+        })
+        this.setState({produtos: this.state.produtos, produtosFiltrados: produtosFiltrados})
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="texto-padrao container ml-sm-0 ml-md-5 mt-5">
+                    <p id="titulo">NOSSOS PRODUTOS</p>
+                    <p> Confira nossas incríveis jóias, todas trabalhadas no
+                mais exigente padrão de qualidade, elaboradas com cuidado e elegância. <br />
+                Aproveite pois estamos no nosso mês de aniversário, <strong> todas as jóias com descontos imperdíveis!</strong>
+                    </p>
+                </div>
+
+                <br />
+
+                <div className="container mb-5 w-50">
+                    <div className="alert alert-warning" role="alert">
+                        <strong>Atenção</strong>, promoção válida apenas para esse mês!
+                </div>
+                    <div className="alert alert-primary" role="alert">
+                        <strong>FRETE GRÁTIS</strong> para compras de 2 jóias ou mais!
+                </div>
+                </div>
+
+                <br />
+                <Container fluid>
+                    <Row>
+                        <Col sm={12} md={4} lg={3}>
+                            <aside className="categorias">
+                                <p>CATEGORIAS</p>
+                                <ul>
+                                    <li onClick={ () => this.exibirCategoria('todos')}>Todas as jóias (15)</li>
+                                    <li onClick={ () => this.exibirCategoria('alianca')}>Alianças (3) </li>
+                                    <li onClick={ () => this.exibirCategoria('anel')}>Anéis (3)</li>
+                                    <li onClick={ () => this.exibirCategoria('colar')}>Colares (3) </li>
+                                    <li onClick={ () => this.exibirCategoria('brinco')}>Brincos (3) </li>
+                                    <li onClick={ () => this.exibirCategoria('pulseira')}>Pulseiras (3) </li>
+                                </ul>
+                            </aside>
+                        </Col>
+                        <Col sm={12} md={8} lg={9}>
+                            {this.state.produtosFiltrados && this.state.produtosFiltrados.map(produto => <Produto produto={produto} />)}
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+
+    )
+
+    }
+
+    
 }
 
 
